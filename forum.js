be0398342232992e458cd490ebc7f83ee2775807
DishankTiwari <dishank@fusioncharts.com>
2015-08-10 18:16:58 +0530

    Elements

--- forum.js
+++ forum.js
@@ -21,44 +21,44 @@ function Forum() {
         parentElement.appendChild(element);
         return element;
     }
-    createElement("div", document.body, {id:"maindiv"}, { boxShadow:"0px 0px 50px deepskyblue" , position:"relative", margin:"auto", height:"auto", width:"60%"}, null, "");
+    createElement("div", document.body, {id:"maindiv"}, { boxShadow:"0px 0px 50px deepskyblue", position:"relative", margin:"auto", height:"auto", width:"60%"}, null, "");
     this.login = function() {
-        createElement("div", maindiv, {id:"insertDiv"}, {position:"relative", height:"20%", width:"100%", backgroundColor:"AliceBlue" , borderBottom: "2px solid black"},null, "");
+        createElement("div", maindiv, {id:"insertDiv"}, {position:"relative", height:"20%", width:"100%", backgroundColor:"AliceBlue", borderBottom:"2px solid black"}, null, "");
         createElement("h1", insertDiv, {id:"heading"}, {position:"absolute", width:"100%", textAlign:"center"}, null, "Online Forum");
         createElement("input", insertDiv, {id:"userId", placeholder:"Please Enter Your ID"}, {position:"absolute", left:"5%", top:"60%", height:"20%", width:"50%"}, null, "");
-        createElement("button", insertDiv, {id:"go1"}, {position:"absolute", right:"30%", top:"60%", height:"20%", width:"10%", cursor: "pointer", backgroundColor:"white"}, null, "GO");
+        createElement("button", insertDiv, {id:"go1"}, {position:"absolute", right:"30%", top:"60%", height:"20%", width:"10%", cursor:"pointer", backgroundColor:"white"}, null, "GO");
         createElement("span", insertDiv, {id:"rol"}, {position:"absolute", right:"2%", top:"10%", color: "#008B8B"}, null, "");
         return {
-                "login_id":"userId",
-                "go1":"go1",
-                "user_type":"rol"
+                "login_id" : "userId",
+                "go1" : "go1",
+                "user_type" : "rol"
                };
     };
     this.addUser = function() {
         if (document.getElementById("addUserDiv") === null) {
-            createElement("div", maindiv, {id:"addUserDiv"}, {position:"relative", height:"15%", width:"100%", backgroundColor:"AliceBlue" , borderBottom: "2px solid black"}, null, "");
-            createElement("input", addUserDiv, {id:"name", placeholder:"Name"}, {position:"absolute", left:"5%", top:"30%", height:"30%",width:"20%"}, null, "");
+            createElement("div", maindiv, {id:"addUserDiv"}, {position:"relative", height:"15%", width:"100%", backgroundColor:"AliceBlue", borderBottom: "2px solid black"}, null, "");
+            createElement("input", addUserDiv, {id:"name", placeholder:"Name"}, {position:"absolute", left:"5%", top:"30%", height:"30%", width:"20%"}, null, "");
             createElement("input", addUserDiv, {id:"id", placeholder:"ID"}, {position:"absolute", left:"30%", top:"30%", height:"30%", width:"20%"},null, "");
             createElement("select", addUserDiv, {id:"role", value:"ROLE"}, {position:"absolute", left:"55%", top:"30%", height:"30%",width:"20%"}, null, "");
-            createElement("option", role, {value:""}, {}, null,"Select A Role");
-            createElement("option", role, {id:"roleAdmin", value:"admin"}, {}, null,"Admin");
-            createElement("option", role, {id:"roleMaster", value:"master"}, {}, null,"Master");
-            createElement("option", role, {id:"roleReporter", value:"reporter"}, {}, null,"Reporter");
-            createElement("option", role, {id:"roleAssignee", value:"assignee"}, {}, null,"Assignee");
-            createElement("button", addUserDiv, {id:"add"}, {position:"absolute", left:"80%", top:"30%", height:"30%",width:"10%", cursor: "pointer", backgroundColor:"white"}, null, "ADD");
+            createElement("option", role, {value:""}, {}, null, "Select A Role");
+            createElement("option", role, {id:"roleAdmin", value:"admin"}, {}, null, "Admin");
+            createElement("option", role, {id:"roleMaster", value:"master"}, {}, null, "Master");
+            createElement("option", role, {id:"roleReporter", value:"reporter"}, {}, null, "Reporter");
+            createElement("option", role, {id:"roleAssignee", value:"assignee"}, {}, null, "Assignee");
+            createElement("button", addUserDiv, {id:"add"}, {position:"absolute", left:"80%", top:"30%", height:"30%",width:"10%", cursor:"pointer", backgroundColor:"white"}, null, "ADD");
         }
         if(document.getElementById("viewDiv") === null){
             createElement("div", maindiv, {id:"viewDiv"}, {position:"relative", width:"100%", display:"block"}, null, "");
         }
         return {
-                "name":"name",
-                "login_id":"id",
-                "role":"role",
-                "add_user":"add",
-                "roleAdmin":"roleAdmin",
-                "roleMaster":"roleMaster",
-                "roleReporter":"roleReporter",
-                "roleAssignee":"roleAssignee"
+                "name" : "name",
+                "login_id" : "id",
+                "role" : "role",
+                "add_user" : "add",
+                "roleAdmin" : "roleAdmin",
+                "roleMaster" : "roleMaster",
+                "roleReporter" : "roleReporter",
+                "roleAssignee" : "roleAssignee"
                };
     };
     this.showTicket = function (id, header, name, description) {   
@@ -67,15 +67,15 @@ function Forum() {
         createElement("input", head, {id:"ticket"+j, value:id, readOnly:"true"}, {position:"absolute", left:"5%", top:"5%", height:"30%", width:"20%"}, null, "");
         createElement("input", head, {id:"header"+j, value:header, readOnly:"true"}, {position:"absolute", left:"30%", top:"5%", height:"30%", width:"30%"}, null, "");
         createElement("input", head, {id:"user"+j, value:name, readOnly:"true"}, {position:"absolute", right:"7%", top:"5%", height:"30%", width:"20%"}, null, "");
-        createElement("button", tick, {id:"cancel"+j}, {position:"absolute", right:"2%", top:"10%", height:"10%", width:"3%", cursor: "pointer", backgroundColor:"white"},{click:function(){deleteTicket(this);}},  "X"); 
-        var statusDiv = createElement("div", tick, {id:"statusDiv"+j}, {position:"absolute", right:"2%", bottom:"62%"}, null, "");
-        createElement("a", statusDiv, {id:"confirm"+j}, {position:"relative", cursor:"pointer"}, { click:(function(){var k=j;return function(){colourRed(k);};})()}, "confirm");
-        createElement("a", statusDiv, {id:"reject"+j}, {position:"relative", cursor:"pointer"}, { click:(function(){var k=j;return function(){colourGreen(k);};})()}, " / reject");
-        createElement("a", statusDiv, {id:"reset"+j}, {position:"relative", cursor:"pointer"}, { click:(function(){var k=j;return function(){resetColor(k);};})()}, " / reset");
+        createElement("button", tick, {id:"cancel"+j}, {position:"absolute", right:"2%", top:"10%", height:"10%", width:"3%", cursor: "pointer", backgroundColor:"white"}, {click:function(){deleteTicket(this);}},  "X"); 
+        var statusDiv = createElement("div", tick, {id:"statusDiv"+j}, {position:"absolute", right:"2%", bottom:"55%"}, null, "");
+        createElement("a", statusDiv, {id:"confirm"+j}, {position:"relative", cursor:"pointer"}, { click:(function(){var k=j;return function(){colourRed(k);};})()}, "Confirm");
+        createElement("a", statusDiv, {id:"reject"+j}, {position:"relative", cursor:"pointer"}, { click:(function(){var k=j;return function(){colourGreen(k);};})()}, " / Reject");
+        createElement("a", statusDiv, {id:"reset"+j}, {position:"relative", cursor:"pointer"}, { click:(function(){var k=j;return function(){resetColor(k);};})()}, " / Reset");
         createElement("button", head, {id:"toggle"+j}, {position:"absolute", left:"48%", bottom:"0%", height:"20%",width:"5%", cursor: "pointer"}, { click:(function(){var k=j;return function(){showdes(k);};})()}, "-");
         var desc = createElement("div", tick, {id:"divDescription"+j}, {position:"absolute", bottom:"10%", height:"40%", width:"100%", display:"none"}, null, "");
         createElement("textarea", desc, {id:"textarea"+j, value:description, readOnly:"true"}, {position:"absolute", top:"10%", height:"80%", width:"80%", margin:"2%"}, null, "");
-        createElement("button", tick, {id:"export1" + j, name:"export"}, {position:"absolute", right:"2%", top:"42%", height:"12%", width:"10%", cursor: "pointer", backgroundColor:"white"},{click:function(){ticket.exportTicket(this,false);}}, "Export");
+        createElement("button", tick, {id:"export1" + j, name:"export"}, {position:"absolute", right:"2%", top:"82%", height:"12%", width:"10%", cursor: "pointer", backgroundColor:"white"},{click:function(){ticket.exportTicket(this,false);}}, "Export");
         j++;
         return {
                 "ticket_id" : "ticket" + j,
@@ -97,35 +97,35 @@ function Forum() {
             alert("permission denied");
     }
     function showdes(i) {
-        if(document.getElementById("divDescription"+i).style.display == "none")
-            document.getElementById("divDescription"+i).style.display = "block";
+        if(document.getElementById("divDescription" + i).style.display == "none")
+            document.getElementById("divDescription" + i).style.display = "block";
         else
-            document.getElementById("divDescription"+i).style.display = "none";
+            document.getElementById("divDescription" + i).style.display = "none";
     }
     function colourRed(i) {
-        if(document.getElementById("confirm"+i))
-            document.getElementById("divHeader"+i).style.background = "#CD5C5C";
+        if(document.getElementById("confirm" + i))
+            document.getElementById("ticketDiv" + i).style.background = "IndianRed";
     }
     function colourGreen(i) {
-        if(document.getElementById("reject"+i))
-            document.getElementById("divHeader"+i).style.background = "#8FBC8F";
+        if(document.getElementById("reject" + i))
+            document.getElementById("ticketDiv" + i).style.background = "DarkSeaGreen";
     }
     function resetColor(i) {
-        if(document.getElementById("reset"+i))
-            document.getElementById("divHeader"+i).style.background = "none";
+        if(document.getElementById("reset" + i))
+            document.getElementById("ticketDiv" + i).style.background = "none";
     }
     this.createTicket = function() {
-        createElement("div", maindiv, {id:"insertionDiv"}, {position:"relative", height:"30%", width:"100%", display:"block", backgroundColor:"AliceBlue" , border:"1px solid #40E0D0"},null,  "");
+        createElement("div", maindiv, {id:"insertionDiv"}, {position:"relative", height:"30%", width:"100%", display:"block", backgroundColor:"AliceBlue" , border:"1px solid #40E0D0"}, null,  "");
         createElement("h4", insertionDiv, {id:"thread"}, {position:"absolute", width:"100%", textAlign:"center"}, null, "Insert A Thread");
-        createElement("input", insertionDiv, {id:"threadHeader", placeholder:"Header"}, {position:"absolute", left:"5%", top:"30%", height:"15%", width:"40%"},null,  "");
-        createElement("textarea", insertionDiv, {id:"description", placeholder:"Description"}, {position:"absolute", left:"5%", top:"50%", height:"35%", width:"70%"},null,  "");
-        createElement("button", insertionDiv, {id:"go2"}, {position:"absolute", left:"78%", top:"65%", height:"20%", width:"5%", cursor: "pointer", backgroundColor:"white"},null,  "GO");
+        createElement("input", insertionDiv, {id:"threadHeader", placeholder:"Header"}, {position:"absolute", left:"5%", top:"30%", height:"15%", width:"40%"}, null,  "");
+        createElement("textarea", insertionDiv, {id:"description", placeholder:"Description"}, {position:"absolute", left:"5%", top:"50%", height:"35%", width:"70%"}, null,  "");
+        createElement("button", insertionDiv, {id:"go2"}, {position:"absolute", left:"78%", top:"65%", height:"20%", width:"5%", cursor: "pointer", backgroundColor:"white"}, null,  "GO");
         createElement("button", insertionDiv, {id:"export2"}, {position:"absolute", right:"2%", top:"69%", height:"15%", width:"10%", cursor: "pointer", backgroundColor:"white"}, null, "Export");
         return {
-                "insert_header":"threadHeader",
-                "insert_description":"description",
-                "insert_button":"go2",
-                "export_all":"export2"
+                "insert_header" : "threadHeader",
+                "insert_description" : "description",
+                "insert_button" : "go2",
+                "export_all" : "export2"
                };
     };
 }
