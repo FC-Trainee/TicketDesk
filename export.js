75e95b39ad73a724fd5e0c71c166b1d4167c58ec
poushaliFc <poushali@fusioncharts.com>
2015-08-10 18:42:35 +0530

    confirm/reject updated

--- export.js
+++ export.js
@@ -5,57 +5,60 @@ function ticketExport() {
     var allTicket = [];
     this.exportTicket = function(element, flag) {
         var ticketValue = {};
+        var i;
         var parent = element.parentNode;
         var divchildren = parent.children;
         var children = divchildren[0].children;
+        var content;
         for (i = 0; i < children.length; i++) {
             var childid = children[i].id;
             var childtype = children[i].type;
             if (childid.substr(0, 6) === "ticket" && childtype === "text") {
-                var content = children[i].value;
+                content = children[i].value;
                 ticketValue.ID = content;
             } else {
                 if (childid.substr(0, 4) === "user" && childtype === "text") {
-                    var content = children[i].value;
+                   content = children[i].value;
 
                     ticketValue.header = content;
                 } else if (childid.substr(0, 6) === "header" && childtype === "text") {
-                    var content = children[i].value;
+                    content = children[i].value;
 
                     ticketValue.reporter = content;
                 }
-            }
+            } 
         }
-        var status = divchildren[0].style.background;
+        var status = parent.style.background;
         if (status.toString() === "rgb(205, 92, 92)") {
-            var content = "confirmed";
+           content = "confirmed";
 
             ticketValue.ticketStatus = content;
 
         } else {
             if (status.toString() === "rgb(143, 188, 143)") {
-                var content = "rejected";
+               content = "rejected";
                 ticketValue.ticketStatus = content;
             } else {
-                var content = "none";
+                content = "none";
                 ticketValue.ticketStatus = content;
             }
         }
-        var children = divchildren[3].children;
-        var content = children[0].value;
+        children = divchildren[3].children;
+        content = children[0].value;
         ticketValue.ticketDescription = content;
         if (flag === false)
             console.log(JSON.stringify(ticketValue));
         else
             allTicket.push(ticketValue);
-    }
+    };
     this.exportAllTicket = function() {
         allTicket = [];
+        var j;
         var children = document.getElementsByName("export");
         for (j = 0; j < children.length; j++) {
             this.exportTicket(children[j], true);
         }
 
         console.log(JSON.stringify(allTicket));
-    }
+    };
 }
